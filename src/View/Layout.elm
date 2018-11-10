afe94a80722e9d105a17dd2216a830789cef0fb2
David Nimmo <dnimmo@gmail.com>
2018-11-10 17:03:24 +0000

    add main layout function

---
+++ src/View/Layout.elm
@@ -0,0 +1,23 @@
+module View.Layout exposing (mainLayout)
+
+import Element exposing (Element, column, fillPortion, none, padding, row, spacing, width)
+
+
+emptyColumn : Int -> Element msg
+emptyColumn portion =
+    column [ width <| fillPortion portion ] [ none ]
+
+
+mainLayout : List (Element msg) -> Element msg
+mainLayout children =
+    row [ spacing 20 ]
+        [ emptyColumn 2
+        , column
+            [ width <| fillPortion 6
+            , padding 60
+            , spacing 20
+            ]
+          <|
+            children
+        , emptyColumn 2
+        ]
